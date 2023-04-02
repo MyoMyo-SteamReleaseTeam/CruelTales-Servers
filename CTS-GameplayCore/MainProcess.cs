c9f2081e859da9f34b6e04c477b36a20523dd833
rifflife <rifflife@naver.com>
2023-04-02 23:29:13 +0900

    package: Add log4net and test

--- CTS-GameplayCore/MainProcess.cs
+++ CTS-GameplayCore/MainProcess.cs
@@ -1,12 +1,31 @@
-﻿using CT.Tools.GetOpt;
+﻿using System;
+using System.Reflection;
+using CT.Tools.GetOpt;
+using log4net;
 
 namespace CTS.Instance
 {
 
 	internal class MainProcess
 	{
+		///private static readonly ILog log = LogManager.GetLogger(typeof(MainProcess));
+		private static readonly ILog log = LogManager.GetLogger(type: MethodBase.GetCurrentMethod()?.DeclaringType);
+
 		static void Main(string[] args)
 		{
+			//Console.SetWindowPosition(0, 0);
+			Console.SetWindowSize(200, 50);
+
+			log.Info("Hello info");
+			log.Error("Test error");
+			log.Warn("Warn test est");
+			log.Debug("Debug debug");
+			log.Fatal("Fatal !!!!!");
+
+			Console.Read();
+
+			return;
+
 			string startOption = "-maxuser 500";
 
 			ServerOption serverOption = new ServerOption();
