583fa7ecf891a228981478754a091eb757e561ce
rifflife <rifflife@naver.com>
2023-04-06 18:06:34 +0900

    feat(CorePatcher): Create CorePatcher bat file to build and run it

---
+++ CT-CorePatcher/CorePatcher.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+
+REM Set character set to UTF-8
+chcp 65001
+
+setlocal
+
+REM Run core patcher
+dotnet run -c Release --xmlPath "../CT-NetworkCore/PacketDefinition/" --outputServer "../CT-NetworkCore/Packets/" -baseNamespace "CT.Packets" -packetTypeName "PacketType" --patchCount "2" --source_0 "../CT-NetworkCore" --target_0 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-NetworkCore" --source_1 "../CT-Tools" --target_1 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-Tools"
+
+pause
