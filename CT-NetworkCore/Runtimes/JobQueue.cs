db9c71bfca93ba2f0e1330dc5ebce4ef6949109c
rifflife <rifflife@naver.com>
2023-04-08 22:33:59 +0900

    feat: Job System 작성중

---
+++ CT-NetworkCore/Runtimes/JobQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT.Network.Runtimes
+{
+	public class JobQueue
+	{
+		public void Push(JobContext jobContext)
+		{
+			
+		}
+	}
+}
