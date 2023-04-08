c7ee33e323cff53223df48e146e044e5ca3fbc38
rifflife <rifflife@naver.com>
2023-04-09 05:06:19 +0900

    fix: Add multi line to CorePatcher.bat

--- CT-CorePatcher/CorePatcher.bat
+++ CT-CorePatcher/CorePatcher.bat
@@ -6,6 +6,18 @@ chcp 65001
 setlocal
 
 REM Run core patcher
-dotnet run -c Release --xmlPath "../CT-NetworkCore/PacketDefinition/" --outputServer "../CT-NetworkCore/Packets/" -baseNamespace "CT.Packets" -packetTypeName "PacketType" --patchCount "2" --source_0 "../CT-NetworkCore" --target_0 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-NetworkCore" --source_1 "../CT-Tools" --target_1 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-Tools" --factoryPath "../CT-NetworkCore/Packets/" --sdispatcher "../CTS-GameplayCore/Packets/" --cdispatcher "../CTC-DummyClient/Packets/" "../../CruelTales-Client/CruelTales-Client/Assets/Scripts/Networks/Packets"
+dotnet run -c Release ^
+--xmlPath "../CT-NetworkCore/PacketDefinition/" ^
+--outputServer "../CT-NetworkCore/Packets/" ^
+-baseNamespace "CT.Packets" ^
+-packetTypeName "PacketType" ^
+--patchCount "2" ^
+--source_0 "../CT-NetworkCore" ^
+--target_0 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-NetworkCore" ^
+--source_1 "../CT-Tools" ^
+--target_1 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-Tools" ^
+--factoryPath "../CT-NetworkCore/Packets/" ^
+--sdispatcher "../CTS-GameplayCore/Packets/" ^
+--cdispatcher "../CTC-DummyClient/Packets/" "../../CruelTales-Client/CruelTales-Client/Assets/Scripts/Networks/Packets"
 
 pause
