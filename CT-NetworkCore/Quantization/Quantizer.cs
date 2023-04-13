5d13d70e8022d499a1b74de94f1e0d8b6273dd90
rifflife <rifflife@naver.com>
2023-04-13 14:04:46 +0900

    feat: 방향 벡터 byte로 양자화

---
+++ CT-NetworkCore/Quantization/Quantizer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace CT.Network.Quantization
+{
+	public static class MathExtension
+	{
+		public const float RADIAN_TO_DEGREE = 180.0f / MathF.PI; 
+		public const float DEGREE_TO_RADIAN = MathF.PI / 180.0f;
+		public const float RADIAN = 180 / MathF.PI;
+
+		public static float DegreeToRadian(float angle)
+		{
+			return angle * RADIAN_TO_DEGREE;
+		}
+
+		public static float RadianToDegree(float angle)
+		{
+			return angle * DEGREE_TO_RADIAN;
+		}
+	}
+
+	public static class Quantizer
+	{
+		public const int PRECISION = 6;
+
+		public const float INVERSE_PI = 1 / MathF.PI;
+		public const float QUANTIZE_RAD = INVERSE_PI * 128;
+		public const float REMAP_RAD_INVERSE = MathF.PI / 128.0f;
+
+		public static byte QuantizeFloatToInt8(float value, float maxRange)
+		{
+			return (byte)(value / maxRange * 256);
+		}
+
+		public static float DequantizeFloatFromInt8(byte value, float maxRange)
+		{
+			return value / 256F * maxRange;
+		}
+
+		public static short QuantizeFloatToShort(float value, float maxRange)
+		{
+			return (short)(maxRange / value + maxRange * 0.5f);
+		}
+
+		public static float DequantizeFloatFromShort(short value, float maxRange)
+		{
+			float range = maxRange * 0.5f;
+			return 1 / range * value - range;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static byte Vec2ToRadByte(Vector2 vec2)
+		{
+			return vec2.Y >= 0 ?
+				(byte)(MathF.Acos(vec2.X) * QUANTIZE_RAD) : 
+				(byte)(MathF.Acos(-vec2.X) * QUANTIZE_RAD + 128);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector2 RadByteToVec2(byte radByte)
+		{
+			if (radByte <= 128)
+			{
+				float x = MathF.Round(MathF.Cos(radByte * REMAP_RAD_INVERSE), PRECISION);
+				float y = MathF.Round(MathF.Sqrt(1 - x * x), PRECISION);
+				return new Vector2(x, y);
+			}
+			else
+			{
+				radByte -= 128;
+				float x = MathF.Round(MathF.Cos(radByte * REMAP_RAD_INVERSE), PRECISION);
+				float y = MathF.Round(MathF.Sqrt(1 - x * x), PRECISION);
+				return new Vector2(-x, -y);
+			}
+		}
+	}
+}
---
+++ CT-Test/Tool/Test_Quantization.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Numerics;
+using CT.Network.Quantization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CT.Test.Tool
+{
+	[TestClass]
+	public class Test_Quantization
+	{
+		[TestMethod]
+		public void Quantization_Angle()
+		{
+			check(new Vector2(1, 0));
+			check(new Vector2(1, 1));
+			check(new Vector2(0, 1));
+			check(new Vector2(-1, 1));
+			check(new Vector2(-1, 0));
+			check(new Vector2(-1, -1));
+			check(new Vector2(0, -1));
+			check(new Vector2(1, -1));
+
+			//for (float x = -1; x <= 1; x += 0.1f)
+			//{
+			//	for (float y = -1; y <= 1; y += 0.1f)
+			//	{
+			//		check(new Vector2(x, y));
+			//	}
+			//}
+
+			void check(Vector2 value)
+			{
+				var vec = Vector2.Normalize(value);
+				var a = Quantizer.Vec2ToRadByte(vec);
+				var qVec = Quantizer.RadByteToVec2(a);
+				Console.WriteLine($"Expected:{vec} / Actual:{qVec}");
+
+				int checkPrecision = 0;
+
+				Assert.AreEqual(MathF.Round(vec.X, checkPrecision),
+								MathF.Round(qVec.X, checkPrecision));
+				Assert.AreEqual(MathF.Round(vec.Y, checkPrecision),
+								MathF.Round(qVec.Y, checkPrecision));
+			}
+		}
+
+		//[TestMethod]
+		//public void Quantization_Print()
+		//{
+		//	printQuantizeAngle(new Vector2(1, 0));
+		//	printQuantizeAngle(new Vector2(1, 1));
+		//	printQuantizeAngle(new Vector2(0, 1));
+		//	printQuantizeAngle(new Vector2(-1, 1));
+		//	printQuantizeAngle(new Vector2(-1, 0));
+		//	printQuantizeAngle(new Vector2(-1, -1));
+		//	printQuantizeAngle(new Vector2(0, -1));
+		//	printQuantizeAngle(new Vector2(1, -1));
+		//	printQuantizeAngle(new Vector2());
+		//}
+
+		private void printQuantizeAngle(Vector2 direction, string name = "vec")
+		{
+			Vector2 vec2 = Vector2.Normalize(direction);
+			byte angle = Quantizer.Vec2ToRadByte(vec2);
+			Console.WriteLine($"{name} : " + vec2);
+			Console.WriteLine($"{name} : " + angle);
+		}
+	}
+}
