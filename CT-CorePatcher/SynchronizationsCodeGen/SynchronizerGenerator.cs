e839f539fc99f68614371d743bbe6f0e30d3c574
rifflife <rifflife@naver.com>
2023-04-24 15:25:30 +0900

    fix: 자동생성 코드 Using 구문 namespace 수정

--- CT-CorePatcher/SynchronizationsCodeGen/SynchronizerGenerator.cs
+++ CT-CorePatcher/SynchronizationsCodeGen/SynchronizerGenerator.cs
@@ -168,7 +168,7 @@ namespace CT.CorePatcher.SynchronizationsCodeGen
 				"using CT.Common.Serialization;",
 				"using CT.Common.Serialization.Type;",
 				"using CT.Common.Synchronizations;",
-				"using CT.Tools.Collections;",
+				"using CT.Common.Tools.Collections;",
 				"using CTS.Instance.Synchronizations;",
 			};
 
@@ -183,7 +183,7 @@ namespace CT.CorePatcher.SynchronizationsCodeGen
 				"using CT.Common.Serialization;",
 				"using CT.Common.Serialization.Type;",
 				"using CT.Common.Synchronizations;",
-				"using CT.Tools.Collections;",
+				"using CT.Common.Tools.Collections;",
 				"using CTC.Networks.Synchornizations;",
 			};
 
