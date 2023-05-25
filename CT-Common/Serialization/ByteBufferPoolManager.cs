1b5609e283f39a61e93c1585fb34eafa9e676f80
rifflife <rifflife@naver.com>
2023-05-25 18:52:56 +0900

    feat: Buffer pool 제작중

---
+++ CT-Common/Serialization/ByteBufferPoolManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT.Common.Serialization
+{
+	public class ByteBufferPoolManager
+	{
+		public int PoolCount { get; private set; }
+		public int BufferSize { get; private set; }
+
+		public ByteBufferPoolManager(int poolCount, int bufferSize)
+		{
+			PoolCount = poolCount;
+			BufferSize = bufferSize;
+		}
+
+	}
+
+	public class ByteBufferPool
+	{
+		public int BufferCount { get; private set; }
+		public int BufferSize { get; private set; }
+		byte[] _buffer;
+
+		public ByteBufferPool(int bufferCount, int bufferSize)
+		{
+			BufferCount = bufferCount;
+			BufferSize = bufferSize;
+		}
+
+		//public ArraySegment<byte> GetBuffer() 
+		//{
+
+		//}
+	}
+}
