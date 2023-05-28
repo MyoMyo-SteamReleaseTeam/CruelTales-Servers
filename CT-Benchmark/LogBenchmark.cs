04618da2f0f3c9035ceec19b7e488d0f91b23baf
rifflife <rifflife@naver.com>
2023-05-28 17:36:12 +0900

    test: 문자열 조합 관련 GC 테스트 진행

--- CT-Benchmark/LogBenchmark.cs
+++ CT-Benchmark/LogBenchmark.cs
@@ -8,10 +8,10 @@ namespace CT.Benchmark
 	public class LogBenchmark
 	{
 		private static readonly ILog _log = LogManager.GetLogger(typeof(LogBenchmark));
-		private string _str0 = "123414214124";
-		private string _str1 = "253151353151";
-		private string _str2 = "656235315151";
-		private string _str3 = "992412414211";
+		private volatile string _str0 = "123414214124fafawfawfaw";
+		private volatile string _str1 = "253151353151fvevevevsfefd32f2vr324vr3rv34";
+		private volatile string _str2 = "656235315151secvwvf2vd2dv321vd23";
+		private volatile string _str3 = "992412414211v12ev1ev21ve12ve12v41241v51356b365246v";
 
 		[Benchmark]
 		public void StaticLogAtConsole()
@@ -42,6 +42,18 @@ namespace CT.Benchmark
 		public void InterpolateLogAtConsole()
 		{
 			_log.Info($"This is {nameof(LogBenchmark)} avev {_str0} avev {_str1} avev {_str2} avev {_str3}");
+		}
+
+		[Benchmark]
+		public void StringFormatLogAtConsole()
+		{
+			_log.InfoFormat(string.Format("This is {0} avev {1} avev {2} avev {3} avev {4}", nameof(LogBenchmark), _str0, _str1, _str2, _str3));
+		}
+
+		[Benchmark]
+		public void LogFormatLogAtConsole()
+		{
+			_log.InfoFormat("This is {0} avev {1} avev {2} avev {3} avev {4}", nameof(LogBenchmark), _str0, _str1, _str2, _str3);
 		}
 	}
 }
