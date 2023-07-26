25bfb58778bc2a438bd725367b3cdb3fab640ae7
rifflife <rifflife@naver.com>
2023-07-26 18:00:40 +0900

    feat: Dokza 애니메이션 및 방향 enum 추가

---
+++ CT-Common/Gameplay/PlayerCharacter/DokzaAnimationState.cs
@@ -0,0 +1,17 @@
+﻿namespace CT.Common.Gameplay.PlayerCharacter
+{
+	public enum DokzaAnimationState : byte
+	{
+		None = 0,
+		Idle,
+		Run,
+		Walk,
+		Jump,
+		Knockback,
+		Stun,
+		Action,
+		AFK,
+		Push,
+		Pushed,
+	}
+}
---
+++ CT-Common/Gameplay/PlayerCharacter/DokzaDirection.cs
@@ -0,0 +1,10 @@
+﻿namespace CT.Common.Gameplay.PlayerCharacter
+{
+	public enum DokzaDirection : byte
+	{
+		RightDown = 0,
+		LeftDown,
+		RightUp,
+		LeftUp
+	}
+}
