ee9768462cacfd30b84b5b264bd88aba62e123b5
rifflife <rifflife@naver.com>
2023-09-18 14:55:20 +0900

    feat: CorePatcher 전체 실행 bat 파일 제작

---
+++ CT-CorePatcher/CorePatcher_Entire.bat
@@ -0,0 +1,39 @@
+﻿@echo off
+
+REM Set character set to UTF-8
+chcp 65001
+
+setlocal
+
+REM Run core patcher : File patcher
+ dotnet run -c Release ^
+ --programName "programXml" "programFilePatch" ^
+ --xmlPath "../CT-Definition/Packets/" ^
+ --packetTypePath "../CT-Common/Packets/" ^
+ --outputServer "../CT-NetworkCore/Packets/" ^
+ -baseNamespace "CT.Packets" ^
+ -packetTypeName "PacketType" ^
+ --patchCount "5" ^
+ --source_0 "../CT-NetworkCore" ^
+ --target_0 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-NetworkCore" ^
+ --source_1 "../CT-Common" ^
+ --target_1 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CT-Common" ^
+ --source_2 "../CTC-Networks" ^
+ --target_2 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/CTC-Networks" ^
+ --source_3 "../KaNetTool" ^
+ --target_3 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/KaNetTool" ^
+ --source_4 "../KaNetPhysics" ^
+ --target_4 "../../CruelTales-Client/CruelTales-Client/Assets/Plugins/KaNetPhysics" ^
+ --factoryServerPath "../CTS-GameplayServer/Packets/" ^
+ --factoryClientPath "../CTC-Networks/Packets/" ^
+ --sdispatcher "../CTS-GameplayServer/Packets/" ^
+ --cdispatcher "../CTC-DummyClient/Packets/" "../../CruelTales-Client/CruelTales-Client/Assets/Scripts/Networks/Packets"
+
+REM Run core patcher : Generate synchronization code
+ dotnet run -c Release ^
+ --programName "programSync" ^
+ --syncMasterPath "../CTS-GameplayServer/SyncObjects/" ^
+ --syncRemotePath "../../CruelTales-Client/CruelTales-Client/Assets/Scripts/Networks/SyncObjects" ^
+ --masterPoolPath "../CTS-GameplayServer/Gameplay/ObjectManagements/" ^
+
+REM pause
