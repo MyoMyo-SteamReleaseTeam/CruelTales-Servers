5a6a9882e413707b60bfef7847832aadf2013245
rifflife <rifflife@naver.com>
2023-09-18 16:05:46 +0900

    feat: Naming format 변경 string extension 작성 및 테스트

--- CT-Common/Tools/Extensions/StringExtension.cs
+++ CT-Common/Tools/Extensions/StringExtension.cs
@@ -1,33 +1,64 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 public static class StringExtension
 {
+	private const int TO_LOWERCASE_OFFSET = ('a' - 'A');
+	private const int TO_UPPERCASE_OFFSET = ('A' - 'a');
+
 	/// <summary>해당 문자열이 알파벳과 숫자로만 이루어져있는지 판단합니다.</summary>
 	/// <param name="data">검사할 문자열</param>
 	/// <returns>해당 문자열이 알파벳과 숫자로만 이루어져있다면 true를 반환합니다.</returns>
-	public static bool IsOnlyAlphabetAndNumber(this string data)
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsOnlyAlphabetOrNumber(this string data)
 	{
 		foreach (char c in data)
 		{
-			if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')))
-			{
+			if (!IsAlphabetOrNumber(c))
 				return false;
-			}
 		}
 
 		return true;
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsAlphabetOrNumber(this char c)
+	{
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsAlphabet(this char c)
+	{
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool IsLowerCase(this char c)
 	{
 		return (c >= 'a' && c <= 'z');
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool IsUpperCase(this char c)
 	{
 		return (c >= 'A' && c <= 'Z');
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static char ToLower(this char c)
+	{
+		return (char)(c + TO_LOWERCASE_OFFSET);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static char ToUpper(this char c)
+	{
+		return (char)(c + TO_UPPERCASE_OFFSET);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static string TryFormat(string format, object arg)
 	{
 		try
@@ -40,6 +71,7 @@ public static class StringExtension
 		}
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static string TryFormat(string format, params object[] args)
 	{
 		try
@@ -50,5 +82,97 @@ public static class StringExtension
 		{
 			return format + e.Message;
 		}
+	}
+
+	public static string ToSnakeCase(this string value)
+	{
+		StringBuilder sb = new(value.Length * 2);
+
+		int s;
+
+		for (s = 0; s < value.Length; s++)
+		{
+			char c = value[s];
+			if (c.IsAlphabet())
+			{
+				sb.Append(c.ToLower());
+				break;
+			}
+		}
+
+		s++;
+
+		for (int i = s; i < value.Length; i++)
+		{
+			char c = value[i];
+
+			if (c.IsUpperCase())
+			{
+				if (i == value.Length - 1)
+				{
+					sb.Append(c);
+					break;
+				}
+
+				sb.Append('_');
+				sb.Append(c.ToLower());
+			}
+			else
+			{
+				sb.Append(c);
+			}
+		}
+
+		return sb.ToString();
+	}
+
+	public static string ToPascalCase(this string value)
+	{
+		StringBuilder sb = new(value.Length * 2);
+
+		int s;
+
+		for (s = 0; s < value.Length; s++)
+		{
+			char c = value[s];
+			if (c.IsAlphabet())
+			{
+				sb.Append(c.ToUpper());
+				break;
+			}
+		}
+
+		s++;
+
+		for (int i = s; i < value.Length; i++)
+		{
+			char c = value[i];
+			if (c == '_')
+			{
+				i++;
+
+				if (i >= value.Length)
+				{
+					sb.Append(c);
+					break;
+				}
+
+				c = value[i];
+				if (c.IsAlphabet())
+				{
+					sb.Append(c.ToUpper());
+				}
+				else
+				{
+					sb.Append(c);
+				}
+			}
+			else
+			{
+				sb.Append(c);
+			}
+		}
+
+		return sb.ToString();
 	}
 }
--- CT-Test/Tool/Extensions/Test_StringExtension.cs
+++ CT-Test/Tool/Extensions/Test_StringExtension.cs
@@ -15,11 +15,27 @@ namespace CT.Test.Tool.Extensions
 				string testString_3 = "한글1235zhjalg^@";
 				string testString_4 = "#@&&&(@^@";
 
-				Assert.IsTrue(testString_1.IsOnlyAlphabetAndNumber());
-				Assert.IsFalse(testString_2.IsOnlyAlphabetAndNumber());
-				Assert.IsFalse(testString_3.IsOnlyAlphabetAndNumber());
-				Assert.IsFalse(testString_4.IsOnlyAlphabetAndNumber());
+				Assert.IsTrue(testString_1.IsOnlyAlphabetOrNumber());
+				Assert.IsFalse(testString_2.IsOnlyAlphabetOrNumber());
+				Assert.IsFalse(testString_3.IsOnlyAlphabetOrNumber());
+				Assert.IsFalse(testString_4.IsOnlyAlphabetOrNumber());
 			}
+		}
+
+		[TestMethod]
+		public void CaseChangeTest()
+		{
+			string pascalCase_0 = "PascalCase";
+			string pascalCase_1 = "PascalCaseLastUpperT";
+
+			string snakeCase_0 = "snake_case_0";
+			string snakeCase_1 = "snake_case_2_str";
+
+			Assert.AreEqual("pascal_case", pascalCase_0.ToSnakeCase());
+			Assert.AreEqual("pascal_case_last_upperT", pascalCase_1.ToSnakeCase());
+
+			Assert.AreEqual("SnakeCase0", snakeCase_0.ToPascalCase());
+			Assert.AreEqual("SnakeCase2Str", snakeCase_1.ToPascalCase());
 		}
 	}
 }
